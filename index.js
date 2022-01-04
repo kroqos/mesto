2522107d240cee64cf0b69d015d73ef81f3fbbf7
kroqos(PC_DSKTP) <kroqos@users.noreply.github.com>
2022-01-04 16:39:50 +0300

    fix: refactor JS-code and fix buttons :focus state

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
+// Объявление всех переменных
 let root = document.querySelector('.root');
+let editForm = root.querySelector('.edit-form');
+let profile = root.querySelector('.profile');
+let popup = root.querySelector('.popup');
+
+let profileName = profile.querySelector('.profile__name');
+let profileAbout = profile.querySelector('.profile__about');
+let editBttn = profile.querySelector('.profile__edit-button');
+
+let formName = editForm.querySelector('.edit-form__input_name');
+let formAbout = editForm.querySelector('.edit-form__input_about');
+
+let closeBttn = popup.querySelector('.popup__close-button');
+
+
 
 // Функция записи информации из профиля в поля ввода формы
-let profileName = root.querySelector('.profile__name');
-let profileAbout = root.querySelector('.profile__about');
-let formName = root.querySelector('.edit-form__input_name');
-let formAbout = root.querySelector('.edit-form__input_about');
-
-function profileInfoToForm() {
+function writeProfileInfoToForm() {
     formName.value = profileName.textContent;
     formAbout.value = profileAbout.textContent;
 }
-// Конец блока
 
 
-
-// Открытие и закрытие попапа
-let editBttn = root.querySelector('.profile__edit-button');
-let closeBttn = root.querySelector('.popup__close-button');
-let popup = root.querySelector('.popup');
-
-function popupOpen() {
+// Функции открытия и закрытия попапа
+function openPopup() {
     popup.classList.add('popup_opened');
-    profileInfoToForm();
+    writeProfileInfoToForm();
 }
 
-editBttn.addEventListener('click', popupOpen);
-
-
-function popupClose() {
+function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
-closeBttn.addEventListener('click', popupClose);
-document.addEventListener('keydown', function(event) {
-    if (event.code === 'Escape') {
-        popupClose();
-    }
-});
-// Конец блока
 
-
-
-// Редактирование информации через форму
-
-let editForm = root.querySelector('.edit-form');
-
+// Функция редактирование информации через форму
 function submitEditingInfo(evt) {
     evt.preventDefault();
     
     profileName.textContent = formName.value;
     profileAbout.textContent = formAbout.value;
-    popupClose();
+    closePopup();
 }
 
+
+
+// Слушатели
+editBttn.addEventListener('click', openPopup);
+
 editForm.addEventListener('submit', submitEditingInfo);
-// Конец блока
+
+closeBttn.addEventListener('click', closePopup);
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'Escape') {
+        closePopup();
+    }
+});
