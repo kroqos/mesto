2c4d44509e6cfb247755391a36c31643416f4004
kroqos(PC_DSKTP) <kroqos@users.noreply.github.com>
2022-01-15 18:14:01 +0300

    feat: make cards section template and create array with content of cards

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const root = document.querySelector('.root');
 const editForm = root.querySelector('.edit-form');
 const profile = root.querySelector('.profile');
 const popup = root.querySelector('.popup');
+const cardsContainer = root.querySelector('.cards-container');
+const cardsTemplate = root.querySelector('.cards-template').content;
+
+const cardsSection = cardsTemplate.querySelector('.cards').cloneNode(false);
+const cardsList = cardsTemplate.querySelector('.grid-elements').cloneNode(false);
 
 const profileName = profile.querySelector('.profile__name');
 const profileAbout = profile.querySelector('.profile__about');
@@ -13,7 +18,53 @@ const formAbout = editForm.querySelector('.edit-form__input_about');
 
 const closeBttn = popup.querySelector('.popup__close-button');
 
+// Массив с начальными карточками
+const initialCards = [
+    {
+        name: 'Карачаевск',
+        imageLink: 'images/cards/card__photo-1.jpg',
+        imageAlt: 'фотография полуразрушенной церкви в Карачаево-Черкесии',
+    },
+    {
+        name: 'Гора Эльбрус',
+        imageLink: 'images/cards/card__photo-2.jpg',
+        imageAlt: 'фотография далекой горы Эльбрус',
+    },
+    {
+        name: 'Домбай',
+        imageLink: 'images/cards/card__photo-3.jpg',
+        imageAlt: 'фотография горы Домбай',
+    },
+    {
+        name: 'Челябинск',
+        imageLink: 'images/cards/card__photo-4.jpg',
+        imageAlt: 'фотография зимней равнины с высокими деревьями',
+    },
+    {
+        name: 'Ивановская область',
+        imageLink: 'images/cards/card__photo-5.jpg',
+        imageAlt: 'фотография небольшого полуострова в туманном водоеме',
+    },
+    {
+        name: 'Шерегеш',
+        imageLink: 'images/cards/card__photo-6.jpg',
+        imageAlt: 'фото зимнего склона с высокими елями',
+    },
+];
 
+// Функция рендеринга первых 6 карточек в массиве, срабатывающая при загрузке страницы
+function renderCards() {
+    initialCards.forEach(card => {
+        const cardElement = cardsTemplate.querySelector('.grid-elements__item').cloneNode(true);
+        cardElement.querySelector('.card__name').textContent = card.name;
+        cardElement.querySelector('.card__photo').src = card.imageLink;
+        cardElement.querySelector('.card__photo').alt = card.imageAlt;
+        cardsContainer.append(cardsSection);
+        cardsSection.append(cardsList);
+        cardsList.append(cardElement);
+    });
+};
+renderCards();
 
 // Функция записи информации из профиля в поля ввода формы
 function writeProfileInfoToForm() {
