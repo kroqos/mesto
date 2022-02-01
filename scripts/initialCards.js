4fcd42d1f3a206d6b287c33376f97c30c7837a84
kroqos <kroqos@users.noreply.github.com>
2022-02-01 15:02:23 +0300

    fix: change initialCards object to fix directories of photos

--- scripts/initialCards.js
+++ scripts/initialCards.js
@@ -2,26 +2,26 @@
 const initialCards = [
     {
         name: 'Карачаевск',
-        imageLink: '../images/cards/card__photo-1.jpg',
+        imageLink: '/images/cards/card__photo-1.jpg',
     },
     {
         name: 'Гора Эльбрус',
-        imageLink: '../images/cards/card__photo-2.jpg',
+        imageLink: '/images/cards/card__photo-2.jpg',
     },
     {
         name: 'Домбай',
-        imageLink: '../images/cards/card__photo-3.jpg',
+        imageLink: '/images/cards/card__photo-3.jpg',
     },
     {
         name: 'Челябинск',
-        imageLink: '../images/cards/card__photo-4.jpg',
+        imageLink: '/images/cards/card__photo-4.jpg',
     },
     {
         name: 'Ивановская область',
-        imageLink: '../images/cards/card__photo-5.jpg',
+        imageLink: '/images/cards/card__photo-5.jpg',
     },
     {
         name: 'Шерегеш',
-        imageLink: '../images/cards/card__photo-6.jpg',
+        imageLink: '/images/cards/card__photo-6.jpg',
     },
 ];
