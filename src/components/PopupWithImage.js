5b96518ce9acc5da166d65ad8c6e842c05247616
Ilia Sagitov <ommazh@protonmail.com>
2022-05-30 21:29:15 +0400

    fix: image title shows up in popup

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -4,14 +4,13 @@ export default class PopupWithImage extends Popup {
   constructor({ popupSelector, popupImageSelector, imageTitleSelector }) {
     super({ popupSelector });
     this._popupPicture = this._popup.querySelector(popupImageSelector);
-    this._pictureTitle =
-      this._popup.querySelector(imageTitleSelector).textContent;
+    this._pictureTitle = this._popup.querySelector(imageTitleSelector);
   }
 
   open(name, pic) {
     super.open();
 
-    this._pictureTitle = name;
+    this._pictureTitle.textContent = name;
     this._popupPicture.src = pic;
     this._popupPicture.alt = name;
   }
